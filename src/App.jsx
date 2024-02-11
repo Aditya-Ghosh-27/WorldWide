b48e3ac4d34f4618ef1adf3854a83572c5149a02
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-12 01:19:05 +0530

    Using CSS modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import AppNav from "./components/AppNav"
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" element={<Homepage />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
+            <Route path="app" element={<AppNav />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
     </BrowserRouter>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>HomePage</h1>
-      <NavLink to="/pricing">Pricing</NavLink>
+      <NavLink to="/app">App navigation</NavLink>
     </div>
   );
 }
