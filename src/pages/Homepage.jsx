9e4ffa1be081611aa5c9f2a6f0ad2e2a5ebca808
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-11 23:06:07 +0530

    linking between routes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,14 @@
+import { NavLink } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
   return (
     <div>
-      HomePage
+      <PageNav />
+      <h1>HomePage</h1>
+      <NavLink to="/pricing">Pricing</NavLink>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
