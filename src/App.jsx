e13db3c3e25cdad84b1e4a45a4d9889b81fd1f97
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-13 18:52:42 +0530

    fetching cities data from the fake api

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,34 @@ import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import { useEffect, useState } from "react";
 
 // Index Route - A child route with no path that renders in the parent's outlet at the parent's URL.
 
+const BASE_URL = "http://localhost:9000";
+
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function(){
+    async function fetchCities(){
+      try{
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+      }
+      catch{
+        alert("There was an error loading data from the API");
+      }
+      finally{
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, [])
+
   return (
     <BrowserRouter>
         <Routes>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,15 @@
+import styles from './CityList.module.css'
+
+function CityList() {
+  return (
+    <ul className={styles.CityList}>
+      
+    </ul>
+  )
+}
+
+export default CityList
+
+
+// How to fetch a fake API?
+// -> Install json-server (npm i json-server)
