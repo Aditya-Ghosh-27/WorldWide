106c4169e7fc4b75e2a3f303b7318fba7e78c117
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-13 18:18:23 +0530

    implemented nested routes and added index routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,22 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 
+// Index Route - A child route with no path that renders in the parent's outlet at the parent's URL.
+
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Homepage />} />
+            <Route index element={<Homepage />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />} />
+            <Route path="app" element={<AppLayout />} >
+              <Route index element={<p>List of cities</p>} />
+              <Route path="cities" element={<p>List of cities</p>} />
+              <Route path="countries" element={<p>Countries</p>} />
+              <Route path="form" element={<p>Form</p>} />
+            </Route>
             <Route path="*" element={<PageNotFound />} />
         </Routes>
     </BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import styles from './Sidebar.module.css'
 import Logo from './Logo';
 import AppNav from './AppNav';
+import { Outlet } from 'react-router-dom';
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-
-      <p>List of cities</p>
+      {/* The Outlet element in React Router DOM is used to render child routes within a parent route. */}
+      <Outlet />
 
       <footer className={styles.footer}>
         <p className={styles.copyright}>&copy; Copyright {new Date().getFullYear()} by WorldWise Inc.</p>
